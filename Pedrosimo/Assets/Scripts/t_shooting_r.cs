b05dc2aac10ca74d377873cc5d321b4b898a09de
Gufis <30979277+GufisYT@users.noreply.github.com>
2019-07-24 21:52:45 +0300

    0.0.33 more fixing
    just more bug fixing/optimizing. Nothing too important :/

--- Pedrosimo/Assets/Scripts/t_shooting_r.cs
+++ Pedrosimo/Assets/Scripts/t_shooting_r.cs
@@ -1,23 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
 
 public class t_shooting_r : MonoBehaviour
 {
+    private SteamVR_Action_Boolean t_Shoot;
+    private SteamVR_Action_Boolean t_SlowTime;
+    private readonly float DesiredInst = 1f;
+    private float CurrentInst;
+    private bool AllowChange;
+
     public SteamVR_ActionSet m_Action;
     public Light SlowEffect;
-    public SteamVR_Action_Boolean t_Shoot;
-    private SteamVR_Action_Boolean t_SlowTime;
     public Pistol_Shooting_r pistolS_r;
-
 
     // Start is called before the first frame update
     void Start()
     {
         t_Shoot = SteamVR_Actions.main_Shoot;
         t_SlowTime = SteamVR_Actions.main_SlowTime;
-        SlowEffect.intensity = 1f;
+        CurrentInst = 0f;
+        AllowChange = false;
     }
 
     // Update is called once per frame
@@ -27,14 +32,42 @@ public class t_shooting_r : MonoBehaviour
         {
             SlowingTime();
         }
+        if (!t_SlowTime.GetState(SteamVR_Input_Sources.RightHand))
+        {
+            NormalTime();
+        }
         if (t_Shoot.GetStateDown(SteamVR_Input_Sources.RightHand))
         {
             pistolS_r.Fire();
         }
     }
 
+
     public void SlowingTime()
     {
-        SlowEffect.intensity -= 0.1f;
+        if(CurrentInst < DesiredInst && AllowChange == false)
+        {
+            CurrentInst += 0.1f;
+            SlowEffect.intensity = CurrentInst;
+        }
+        if(CurrentInst >= DesiredInst)
+        {
+            Time.timeScale = 0.25f;
+            AllowChange = true;
+        }
+    }
+
+    public void NormalTime()
+    {
+        if (CurrentInst >= 0f)
+        {
+            CurrentInst -= 0.1f;
+            SlowEffect.intensity = CurrentInst;
+        }
+        if (CurrentInst < 0.1f && AllowChange == true)
+        {
+            Time.timeScale = 1f;
+            AllowChange = false;
+        }
     }
 }
